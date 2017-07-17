895c3972c9ffb392352d1320fe4f5df4ae0df3ae
Boro_s99 <Boro_s99@221-12.od.itstep.local>
2017-07-17 19:37:14 +0300

    Getter and Setter

--- src/eleventh_project/Eleventh_project.java
+++ src/eleventh_project/Eleventh_project.java
@@ -25,6 +25,42 @@ class Car
             this.name = name;
         }
     }
+class Car2
+    {
+        private  int Speed;
+        private String name;
+        
+        void Print()
+        {
+            System.out.println(this.name);
+        }
+        Car2() //Дефолтный конструктор
+        {
+            Speed = 0;
+            name = "none";
+        }
+        Car2(int Speed, String name) //Конструктор с параметрами
+        {
+            this.Speed = Speed;
+            this.name = name;
+        }
+        public void setSpeed(int sp)
+        {
+            if (sp < 0)
+            {
+                Speed = 0;
+            }
+            else
+            {
+                Speed = sp;
+            }
+        }
+        public int getSpeed()
+        {
+            return Speed;
+            
+        }
+    }
 
 public class Eleventh_project {
 
@@ -32,7 +68,6 @@ public class Eleventh_project {
         {
             if (num <= 1)
             {
-                System.out.println(num + " here");
                 return  1;
             }
             return Fact(num - 1) * num;
@@ -48,7 +83,11 @@ public class Eleventh_project {
         Car car2 = new Car(50, "McLaren");
         car1.Print();
         car2.Print();*/
-        Fact(5);
+       // Fact(5);
+        Car2 car4 = new Car2();
+        car4.setSpeed(-5);
+        System.out.println(car4.getSpeed());
+        
     }
     
 }
