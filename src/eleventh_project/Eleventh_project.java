6b35eb9799bb588d9fb1737dc48aa98cce25b3e4
Boro_s99 <Boro_s99@221-12.od.itstep.local>
2017-07-17 19:18:32 +0300

    recursion

--- src/eleventh_project/Eleventh_project.java
+++ src/eleventh_project/Eleventh_project.java
@@ -5,17 +5,50 @@
  */
 package eleventh_project;
 
-/**
- *
- * @author Boro_s99
- */
+class Car
+    {
+        int maxSpeed;
+        String name;
+        
+        void Print()
+        {
+            System.out.println(this.name);
+        }
+        Car() //Дефолтный конструктор
+        {
+            maxSpeed = 0;
+            name = "none";
+        }
+        Car(int maxSpeed, String name) //Конструктор с параметрами
+        {
+            this.maxSpeed = maxSpeed;
+            this.name = name;
+        }
+    }
+
 public class Eleventh_project {
 
+   static int Fact (int num)
+        {
+            if (num <= 1)
+            {
+                System.out.println(num + " here");
+                return  1;
+            }
+            return Fact(num - 1) * num;
+            //System.out.println(Fact);
+        }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        /* Car car1 = null;
+        car1 = new Car();
+        Car car2 = new Car(50, "McLaren");
+        car1.Print();
+        car2.Print();*/
+        Fact(5);
     }
     
 }
